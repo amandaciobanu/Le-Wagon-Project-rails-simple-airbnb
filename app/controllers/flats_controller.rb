2a8454e1096dd4f7aa27b5cfdd612d2528f0891a
Amanda <amanda@ciobanu.org>
2020-06-17 18:18:18 +0100

    views/controler/ routes change not ready yet

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,59 +1,51 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
-  # GET /flats
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name iLike '%#{params[:query]}%'")
+    else
+      @flats = Flat.all
+    end
   end
 
-  # GET /flats/1
-  def show
-  end
-
-  # GET /flats/new
   def new
     @flat = Flat.new
   end
 
-  # GET /flats/1/edit
-  def edit
-  end
-
-  # POST /flats
   def create
     @flat = Flat.new(flat_params)
-
     if @flat.save
-      redirect_to @flat, notice: 'Flat was successfully created.'
+      redirect_to flats_path
     else
       render :new
     end
   end
 
-  # PATCH/PUT /flats/1
-  def update
-    if @flat.update(flat_params)
-      redirect_to @flat, notice: 'Flat was successfully updated.'
-    else
-      render :edit
-    end
+  def show
   end
 
-  # DELETE /flats/1
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
   def destroy
     @flat.destroy
-    redirect_to flats_url, notice: 'Flat was successfully destroyed.'
+    redirect_to flats_path
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_flat
     @flat = Flat.find(params[:id])
   end
-  # Only allow a trusted parameter "white list" through.
 
   def flat_params
-    params.require(:flat).permit(:name, :adress, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
